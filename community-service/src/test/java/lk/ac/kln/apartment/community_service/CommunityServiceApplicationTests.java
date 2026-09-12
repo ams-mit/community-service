315f4b60423a54958e02b6877c313619341622d2
Sajana Jayawardhana <dewmisajana2023@gmail.com>
2026-09-12 16:23:38 +0530

    Initial Spring Boot project setup with MySQL connection

---
+++ community-service/src/test/java/lk/ac/kln/apartment/community_service/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.kln.apartment.community_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
