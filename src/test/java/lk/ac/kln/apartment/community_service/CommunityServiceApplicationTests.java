bad6899459400e06b12f18ef62321987d49710ec
Kaveesha Vimukthi <kaveeshavimukthiai@gmail.com>
2026-09-26 15:28:04 +0530

    Activate the H2 test profile for Spring Boot context tests

--- src/test/java/lk/ac/kln/apartment/community_service/CommunityServiceApplicationTests.java
+++ src/test/java/lk/ac/kln/apartment/community_service/CommunityServiceApplicationTests.java
@@ -2,8 +2,10 @@ package lk.ac.kln.apartment.community_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CommunityServiceApplicationTests {
 
 	@Test
